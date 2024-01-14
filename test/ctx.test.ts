c1de1ee141f0f1337fce4104e7621f8374413acd
Arkadiy <arkadii.levin@icloud.com>
2024-01-14 15:15:44 +0100

    showList tested

--- test/ctx.test.ts
+++ test/ctx.test.ts
@@ -1,8 +1,11 @@
-import { describe, expect, test, beforeAll, afterAll } from '@jest/globals'
+import { describe, expect, test, beforeAll, beforeEach, afterAll } from '@jest/globals'
 import addToList from '../src/commands/addToList'
 import { CommandContext } from 'grammy'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { connectMongo, disconnectMongo } from '../src/mongo'
+import showList from '../src/commands/showList'
+import ListItem from '../src/models/ListItem'
+import { saveListItem } from '../src/services/persistence'
 
 class ContextMock {
   replies: string[] = []
@@ -23,17 +26,34 @@ describe('Context tests', () => {
     await connectMongo(uri)
   })
 
-  test('empty addToList', async () => {
+  beforeEach(async () => {
+    await ListItem.deleteMany()
+  })
+
+  test('Responds to addToList with no argument', async () => {
     const ctx = new ContextMock() as unknown as CommandContext<any>
     await addToList(ctx)
     expect(ctx.replies[0]).toBe('Please specify what item you would like to add')
   })
 
-  test('item addToList', async () => {
+  test('Responds to showList on empty list', async () => {
+    const ctx = new ContextMock() as unknown as CommandContext<any>
+    await showList(ctx)
+    expect(ctx.replies[0]).toBe('Your list is empty')
+  })
+
+  test('Adds item to list on addToList with one item argument', async () => {
     const ctx = new ContextMock() as unknown as CommandContext<any>
     ctx.match = 'Milk 1'
     await addToList(ctx)
     expect(ctx.replies[0]).toBe('Added Milk of quantity 1')
+  })
+
+  test('Displays bulleted list with items on showList', async () => {
+    const ctx = new ContextMock() as unknown as CommandContext<any>
+    await saveListItem('Cheese', 1)
+    await showList(ctx)
+    expect(ctx.replies[0]).toBe('- 1 Cheese')
   })
 
   afterAll(async () => {
